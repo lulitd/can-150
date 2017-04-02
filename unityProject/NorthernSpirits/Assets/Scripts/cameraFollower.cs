ba5d135a08c53d9a7c9a1739fc6a65162d6de576
LaLaine Ulit-Destajo <lulitdestajo@gmail.com>
2017-04-02 00:34:08 -0400

    updated website to new theme

---
+++ unityProject/NorthernSpirits/Assets/Scripts/cameraFollower.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraFollower : MonoBehaviour
+{
+
+    public float smoothTime = 0.3F;
+
+    [SerializeField]
+    private Transform target;
+
+    [SerializeField]
+    private Vector3 offsetPosition;
+
+    [SerializeField]
+    private Space offsetPositionSpace = Space.Self;
+
+    [SerializeField]
+    private bool lookAt = true;
+
+    new Transform transform; // caching transform
+
+    private Vector3 velocity = Vector3.zero;
+       
+    private void Awake() {
+        transform = GetComponent<Transform>(); 
+    }
+
+    // Late update occurs after fixedUpdate(physics stuff) 
+    private void LateUpdate()
+    {
+        Refresh();
+    }
+
+    public void Refresh()
+    {
+        if (target == null)
+        {
+            Debug.LogWarning("Missing target ref !", this);
+
+            return;
+        }
+
+        // compute position
+        if (offsetPositionSpace == Space.Self)
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, target.TransformPoint(offsetPosition), ref velocity, smoothTime);
+            
+        }
+        else
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, target.TransformPoint(offsetPosition)+ offsetPosition, ref velocity, smoothTime);
+        }
+
+        // compute rotation
+        if (lookAt)
+        {
+            transform.LookAt(target);
+        }
+        else
+        {
+            transform.rotation = target.rotation;
+        }
+    }
+}
---
+++ unityProject/NorthernSpirits/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class playerMovement : MonoBehaviour {
+
+    public float movementSpeed;
+    public float rotateSpeed = 3.0F;
+    CharacterController m_charController;
+
+    string H_axis = "Horizontal";
+    string V_axis = "Vertical";
+
+    // Use this for initialization
+    void Start () {
+        m_charController = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0, Input.GetAxis(H_axis) * rotateSpeed, 0);
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        float curSpeed = movementSpeed * Input.GetAxis(V_axis);
+        m_charController.SimpleMove(forward * curSpeed);
+    }
+}
