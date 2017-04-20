43490b194a648591dc62ef964eb41e7141fa31bc
shgfl <shgfl@hotmail.com>
2017-04-20 19:25:17 -0400

    update

---
+++ unityProject/NorthernSpirits/Assets/moveTo.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class moveTo : MonoBehaviour {
+
+	public Vector3 goal;
+
+	bool LeftClick;
+	Vector3 mousePos;
+	public Camera mainCam;
+	NavMeshAgent agent;
+
+	void Start(){
+		agent = GetComponent<NavMeshAgent>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		LeftClick = Input.GetMouseButtonDown (0);
+		if (LeftClick) {
+			mousePos = Input.mousePosition;
+			//do raycast
+			RaycastHit vHit;
+			Ray vRay = mainCam.ScreenPointToRay (mousePos);
+
+			if (Physics.Raycast (vRay, out vHit)) {
+				if (vHit.collider.gameObject.tag == "Ground") {
+					Debug.Log ("player move!!");
+					goal = vHit.point;
+					agent.destination = goal;
+				}
+			}
+		}
+	}
+}
