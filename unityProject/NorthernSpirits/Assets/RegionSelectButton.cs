bb151631cea95015b4185064d571d9c57480e036
LaLaine Ulit-Destajo <lulitdestajo@gmail.com>
2017-05-01 16:47:50 -0400

    updated main menu
    ->with scene selection( not fully implemented. Will load scene based on
    region name)
    ->base region preview window -> name, sprite, and collected gems.

---
+++ unityProject/NorthernSpirits/Assets/RegionSelectButton.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class RegionSelectButton : MonoBehaviour {
+
+
+    // covert all to single scriptable object?
+    public RegionName Region;
+    public Sprite regionImage;
+
+    public GameObject RegionDisplay;
+    public Text regionLabel;
+    public Text regionStats;
+    public Image imageLabel; 
+
+
+    void Awake() {
+        gameObject.name = "Gem_" + Region.ToString();
+    }
+
+    public void DisplayRegionInfo() {
+
+        regionLabel.text = Region.ToString();
+
+        RegionDisplay.SetActive(true);
+
+    }
+
+    public void LoadRegionScene() {
+
+        if (SceneListCheck.Has(Region.ToString()))
+        {
+
+            SceneManager.LoadScene(Region.ToString());
+
+        }
+        else {
+
+            Debug.LogWarningFormat("Scene {0} does not exist. Please check build settings", Region.ToString());
+
+        }
+        
+    }
+}
