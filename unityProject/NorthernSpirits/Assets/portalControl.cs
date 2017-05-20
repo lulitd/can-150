9024fe7853fd444c71fc77ccbae697b6cf6607f6
shgfl <shgfl@hotmail.com>
2017-05-20 10:38:49 -0400

    update
    portal update

---
+++ unityProject/NorthernSpirits/Assets/portalControl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class portalControl : MonoBehaviour {
+
+	public RegionName teleportRegion;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider col){
+		if (col.gameObject.tag == "Player") {
+			Debug.Log ("player hit portal!");
+			LoadRegionScene ();
+		}
+	}
+
+	public void LoadRegionScene() {
+
+		if (SceneListCheck.Has(teleportRegion.ToString()))
+		{
+
+
+			// TODO add map manager
+
+			SceneManager.LoadScene(teleportRegion.ToString());
+
+		}
+		else {
+
+			Debug.LogWarningFormat("Scene {0} does not exist. Please check build settings", teleportRegion.ToString());
+
+		}
+
+	}
+}
