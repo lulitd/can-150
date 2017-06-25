2e6159533bbf607f2076ba805410f042b5c2e69b
shgfl <shgfl@hotmail.com>
2017-06-25 00:36:19 -0400

    UI Script update

---
+++ unityProject/NorthernSpirits/Assets/Scripts/UIControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIControl : MonoBehaviour {
+	//this class handles ui interactions
+	public GameObject OptionMenu;
+	public GameObject InventoryPanel;
+	public GameObject[] gemList;
+	public GameObject bagIcon;
+
+	public void OpenOptionMenu(){
+		OptionMenu.SetActive (true);
+	}
+
+	public void CloseOptionMenu(){
+		OptionMenu.SetActive (false);
+	}
+
+	public void OpenInventory(){
+		InventoryPanel.SetActive (true);
+		bagIcon.SetActive (false);
+	}
+
+	public void CloseInventory(){
+		InventoryPanel.SetActive (false);
+		bagIcon.SetActive (true);
+	}
+
+	void Update(){
+		//need a function to check and show collected gems
+	}
+}
