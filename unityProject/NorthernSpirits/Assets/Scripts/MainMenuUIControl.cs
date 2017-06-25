e50527d48932472abc3367d1706606b3c6357fbe
shgfl <shgfl@hotmail.com>
2017-06-25 11:54:07 -0400

    main menu added
    Added a main menu

---
+++ unityProject/NorthernSpirits/Assets/Scripts/MainMenuUIControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuUIControl : MonoBehaviour {
+	//this class handles the buttons on the main menu page
+
+	public GameObject OptionPanel;
+
+	public void OpenOption(){
+		OptionPanel.SetActive (true);
+	}
+
+	public void CloseOption(){
+		OptionPanel.SetActive (false);
+	}
+}
