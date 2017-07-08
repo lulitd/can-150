17a7413dd950c3a16a152c4dc549e01e205124ed
shgfl <shgfl@hotmail.com>
2017-07-08 00:47:16 -0400

    opening update

--- unityProject/NorthernSpirits/Assets/Scripts/MainMenuUIControl.cs
+++ unityProject/NorthernSpirits/Assets/Scripts/MainMenuUIControl.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuUIControl : MonoBehaviour {
 	//this class handles the buttons on the main menu page
@@ -13,5 +14,21 @@ public class MainMenuUIControl : MonoBehaviour {
 
 	public void CloseOption(){
 		OptionPanel.SetActive (false);
+	}
+
+	public void ToggleOption(){
+		if (OptionPanel.activeInHierarchy) {
+			OptionPanel.SetActive (false);
+		} else {
+			OptionPanel.SetActive (true);
+		}
+	}
+
+	public void NewGame(){
+		SceneManager.LoadScene (1);
+	}
+
+	public void Exit(){
+		Application.Quit ();
 	}
 }
